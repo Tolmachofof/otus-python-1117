9e622820082106c7edf5874f851cf1c738b1a144
evtolmachev <tolmachofof@gmail.com>
2017-11-29 15:58:05 +0300

    Updated log_analyzer

--- hw1/log_analyzer/log_analyzer.py
+++ hw1/log_analyzer/log_analyzer.py
@@ -232,7 +232,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--config', help='Path to the configuration file.',
-        default='/usr/local/etc/log_nalyzer.conf'
+        default='/usr/local/etc/log_analyzer.conf'
     )
     args = parser.parse_args()
     config_path = args.config
